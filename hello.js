1457f5ab09ad4e601e372039422d8eaa1983cd4e
JCyan90 <julien.atanassian@gmail.com>
2019-08-06 19:39:17 +0000

    Cancelled the comment signs

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
-/*const sayHello  = function () {
+const sayHello  = function () {
   console.log("Hello, world");
 }
 
 sayHello();
-*/
+
 
 const sayHello  = function (name) {
   console.log("Hello, " + name);
