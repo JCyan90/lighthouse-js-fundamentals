1b5a989e24862976012ab67bec92188c5a772b79
JCyan90 <julien.atanassian@gmail.com>
2019-08-07 19:30:11 +0000

    Used a while loop to do the same task as with a for loop

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,15 @@
+// Using a for loop to look over an array one item at a time
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+// Using a while loop works too
+/* console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+} */
